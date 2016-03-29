b8c32dbbbaca3c12538b78bcc3c6520378024e41
pkdash <pkdash_reena@hotmail.com>
2016-03-28 23:28:05 -0600

    initial changes to settings to speedup unit test run

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -3,7 +3,10 @@ from __future__ import absolute_import, unicode_literals
 TEST_RUNNER = 'hs_core.tests.runner.CustomTestSuiteRunner'
 
 import os
+import sys
 import importlib
+
+import logging
 
 local_settings_module = os.environ.get('LOCAL_SETTINGS', 'hydroshare.local_settings')
 
@@ -546,3 +549,24 @@ LOGGING = {
         },
     }
 }
+
+
+class DisableMigrations(object):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+
+TESTS_IN_PROGRESS = False
+if 'test' in sys.argv[1:] or 'jenkins' in sys.argv[1:]:
+    logging.disable(logging.CRITICAL)
+    PASSWORD_HASHERS = (
+        'django.contrib.auth.hashers.MD5PasswordHasher',
+    )
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+    TESTS_IN_PROGRESS = True
+    MIGRATION_MODULES = DisableMigrations()

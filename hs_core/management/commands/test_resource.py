e36eab151c451ffe4e521d04d52cb7f261b0bb0c
Alva Couch <acouch@cuahsi.org>
2017-12-07 09:17:39 -0500

    Improve test output

--- hs_core/management/commands/test_resource.py
+++ hs_core/management/commands/test_resource.py
@@ -8,64 +8,80 @@ It checks iRODS synchronization, logical files, and bag function
 
 from django.core.management.base import BaseCommand
 from hs_core.models import BaseResource
+from django_irods.icommands import SessionException
 
 
-class TestHeader(object):
+class TestResource(object):
     header = False
 
-    def __init__(self, resource):
-        self.resource = resource
+    def __init__(self, short_id):
+        self.short_id = short_id
 
     def label(self):
         if not self.header:
             print("resource {}:".format(self.resource.short_id))
             self.header = True
 
+    def test_avu(self, label):
+        try:
+            value = self.resource.getAVU(label)
+            if value is None:
+                self.label()
+                print("  AVU {} is None".format(label))
+            return value
+        except SessionException:
+            self.label()
+            print("  AVU {} WAS NOT FOUND.".format(label))
+            return None
 
-def test_resource(short_id):
-    """ Test view for resource depicts output of various integrity checking scripts """
+    def test(self):
+        """ Test view for resource depicts output of various integrity checking scripts """
 
-    # print("TEST {}".format(short_id))
-    try:
-        res = BaseResource.objects.get(short_id=short_id)
-    except BaseResource.DoesNotExist:
-        print("{} does not exist".format(short_id))
+        print("TEST {}".format(self.short_id))
+        try:
+            res = BaseResource.objects.get(short_id=self.short_id)
+        except BaseResource.DoesNotExist:
+            print("{} does not exist in Django".format(self.short_id))
 
-    resource = res.get_content_model()
-    assert resource, (res, res.content_model)
+        self.resource = res.get_content_model()
+        assert self.resource, (res, res.content_model)
 
-    head = TestHeader(resource)
+        istorage = self.resource.get_irods_storage()
 
-    if resource.getAVU('bag_modified') is None:
-        head.label()
-        print("  bag_modified is None")
-    if resource.getAVU('isPublic') is None:
-        head.label()
-        print("  isPublic is None")
-    if resource.getAVU('resourceType') is None:
-        head.label()
-        print("  resourceType is None")
-    if resource.getAVU('quotaUserName') is None:
-        head.label()
-        print("  quotaUserName is None")
+        if not istorage.exists(self.resource.root_path):
+            self.label()
+            print("  root path {} does not exist in iRODS".format(self.resource.root_path))
+            return
 
-    irods_issues, irods_errors = resource.check_irods_files(log_errors=False, return_errors=True)
+        for a in ('bag_modified', 'isPublic', 'resourceType', 'quotaUserName'):
+            value = self.test_avu(a)
+            if value is not None and a == 'resourceType' and value != self.resource.resource_type:
+                self.label()
+                print("  resourceType is {}, should be {}".format(value,
+                                                                  self.resource.resource_type))
+            if a == 'isPublic' and value is not None and value != self.resource.raccess.public:
+                self.label()
+                print("  isPublic AVU is {}, but public is {}".format(value,
+                                                                      self.resource.raccess.public))
 
-    if irods_errors:
-        head.label()
-        print("  iRODS errors:")
-        for e in irods_issues:
-            print("    {}".format(e))
+        irods_issues, irods_errors = self.resource.check_irods_files(log_errors=False,
+                                                                     return_errors=True)
 
-    logical_header = False
+        if irods_errors:
+            self.label()
+            print("  iRODS errors:")
+            for e in irods_issues:
+                print("    {}".format(e))
 
-    if resource.resource_type == 'CompositeResource':
-        for res_file in resource.files.all():
-            if not res_file.has_logical_file:
-                head.label()
-                if not logical_header:
-                    print("  logical file errors:")
-                print("    {} logical file NOT SPECIFIED".format(res_file.short_path))
+        logical_header = False
+
+        if self.resource.resource_type == 'CompositeResource':
+            for res_file in self.resource.files.all():
+                if not res_file.has_logical_file:
+                    self.label()
+                    if not logical_header:
+                        print("  logical file errors:")
+                    print("    {} logical file NOT SPECIFIED".format(res_file.short_path))
 
 
 class Command(BaseCommand):
@@ -87,7 +103,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if len(options['resource_ids']) > 0:  # an array of resource short_id to check.
             for rid in options['resource_ids']:
-                test_resource(rid)
+                TestResource(rid).test()
         else:
             for r in BaseResource.objects.all():
-                test_resource(r.short_id)
+                TestResource(r.short_id).test()
